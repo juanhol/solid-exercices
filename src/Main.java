4ecc7f8315cbf5b52d349647d0cd3d80c4943435
juanhol <juanhol93@gmail.com>
2025-03-27 17:12:32 -0300

    second update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+   /* 1 --principio de responsabilidad unica: deberia separar la impresion del invoice
+-- principio de inversion de dependencias: no deberia printear con sout deberia devolvver un string
+*/
+    class Invoice {
+        private double amount;
+
+        public Invoice(double amount) {
+            this.amount = amount;
+        }
+
+        public double getAmount() {
+            return amount;
+        }
     }
+
+    class InvoicePrinter {
+        public String printInvoice(Invoice invoice) {
+            return "Factura por: " + invoice.getAmount();
+        }
+    }
+
+//2 --
+    interface Shape{
+        double getArea();
+    }
+
+    class Circle implements Shape{
+        private double radius;
+
+        public Circle(double radius){this.radius=radius}
+
+        @Override
+        public double getArea(){
+
+        }
+    }
+
 }
